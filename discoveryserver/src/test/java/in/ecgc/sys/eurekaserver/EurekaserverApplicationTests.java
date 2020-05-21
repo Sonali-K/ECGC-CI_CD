6521a0aee7a3105deec7903a30acca002aed868f
sonali-k <sonali.kanse021@gmail.com>
2020-05-21 11:39:42 +0530

    add all data

---
+++ discoveryserver/src/test/java/in/ecgc/sys/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,15 @@
+package in.ecgc.sys.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@SpringBootConfiguration
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
